d221bd186ccabaa4940304ca5b24b5b65a37dd23
lsl <ux_lsl@163.com>
2017-03-05 12:01:56 +0800

    增加markdown模式同步模式

--- sync_evernote.py
+++ sync_evernote.py
@@ -6,6 +6,7 @@ import shutil
 import subprocess
 import binascii
 import hashlib
+import markdown2
 import evernote.edam.userstore.constants as UserStoreConstants
 from evernote.edam.limits import constants as LimitsConstants
 import evernote.edam.notestore.NoteStore as NoteStore
@@ -40,61 +41,76 @@ if not version_ok:
 note_store = client.get_note_store()
 
 
-def create_png_note(title, png_path,notebook):
+def make_resource(path, mime):
+    body = open(path, 'rb').read()
+    md5 = hashlib.md5()
+    md5.update(body)
+    hash = md5.digest()
+    data = Types.Data()
+    data.size = len(body)
+    data.bodyHash = hash
+    data.body = body
+    resource = Types.Resource()
+    resource.mime = mime
+    resource.data = data
+    return resource
+
+
+def create_note(title, notebook,png_path,  markdown_path, html_path):
     """
     `title`: 标题
     `filename`: 文件名
     """
     note = Types.Note()
     note.notebookGuid = notebook.guid
-    image = open(png_path, 'rb').read()
-    md5 = hashlib.md5()
-    md5.update(image)
-    hash = md5.digest()
-    data = Types.Data()
-    data.size = len(image)
-    data.bodyHash = hash
-    data.body = image
-    resource = Types.Resource()
-    resource.mime = 'image/png'
-    resource.data = data
-    note.resources = [resource]
-    hash_hex = binascii.hexlify(hash)
+    note.resources = []
+    png_resource = make_resource(png_path, "image/png")
+    note.resources.append(png_resource)
+
+    if os.path.exists(markdown_path):
+        markdown_resource = make_resource(markdown_path, "text/plain")
+        note.resources.append(markdown_resource)
+
+    if os.path.exists(html_path):
+        html_resource = make_resource(html_path, "text/html")
+        note.resources.append(html_resource)
+
     note.title = title
     note.content = '<?xml version="1.0" encoding="UTF-8"?>'
     note.content += '<!DOCTYPE en-note SYSTEM ' \
         '"http://xml.evernote.com/pub/enml2.dtd">'
     note.content += '<en-note>'
-    note.content += '<en-media type="image/png" hash="' + hash_hex + '"/>'
+    for resource in note.resources:
+        note.content += '<en-media type="' + resource.mime + '" hash="' + binascii.hexlify(resource.data.bodyHash)+ '"/>'
     note.content += '</en-note>'
     return note_store.createNote(note)
 
 
-def update_png_note(note, title, png_path):
+def update_note(note, title, png_path, markdown_path, html_path):
     """
     `title`: 标题
     `filename`: 文件名
     """
-    image = open(png_path, 'rb').read()
     note.updated = int(time.time() * 1000)
-    md5 = hashlib.md5()
-    md5.update(image)
-    hash = md5.digest()
-    data = Types.Data()
-    data.size = len(image)
-    data.bodyHash = hash
-    data.body = image
-    resource = Types.Resource()
-    resource.mime = 'image/png'
-    resource.data = data
-    note.resources = [resource]
-    hash_hex = binascii.hexlify(hash)
+    note.resources = []
+    png_resource = make_resource(png_path, "image/png")
+    note.resources.append(png_resource)
+
+    if os.path.exists(markdown_path):
+        markdown_resource = make_resource(markdown_path, "text/plain")
+        note.resources.append(markdown_resource)
+
+    if os.path.exists(html_path):
+        html_resource = make_resource(html_path, "text/html")
+        note.resources.append(html_resource)
+
     note.title = title
     note.content = '<?xml version="1.0" encoding="UTF-8"?>'
     note.content += '<!DOCTYPE en-note SYSTEM ' \
         '"http://xml.evernote.com/pub/enml2.dtd">'
     note.content += '<en-note>'
-    note.content += '<en-media type="image/png" hash="' + hash_hex + '"/>'
+    for resource in note.resources:
+        note.content += '<en-media type="' + resource.mime + '" hash="' + binascii.hexlify(resource.data.bodyHash)+ '"/>'
     note.content += '</en-note>'
     return note_store.updateNote(auth_token, note)
 
@@ -106,6 +122,22 @@ def convert_to_png(i, o):
     os.remove(t)
 
 
+def convert_to_html(title, i, o):
+    """把markdown 文件转换成html文件"""
+    body = markdown2.markdown_path(i)
+    with open(o, 'w') as f:
+        f.write('''
+                <html>
+	            <head>
+	                <meta charset="utf-8">
+                    <title>{}</title>
+	            </head>
+                        <body>
+                        {}
+                        </body>
+                </html>'''.format(title, body.encode('utf-8')))
+
+
 def sync_evernotes(notebook, path):
     """同步当前目录的笔记到evernote notebook上"""
     _filter = NoteStore.NoteFilter()
@@ -113,29 +145,57 @@ def sync_evernotes(notebook, path):
     spec = NoteStore.NotesMetadataResultSpec()
     spec.includeTitle = True
     notelist = note_store.findNotesMetadata(_filter, 0,LimitsConstants.EDAM_USER_NOTES_MAX, spec)
-    notes = {i.title:i for i in notelist.notes}
+    notes = {i.title:i for i in notelist.notes if i.deleted is None}
 
-    for filename in os.listdir(path):
-        notename, fmt = filename.split('.')
-        if fmt != 'html':
-            continue
-        if notename in notes:
-            note = notes[notename]
-            file_stat = os.stat(os.path.join(path, filename))
+    local_notenames = set([i.split('.')[0] for i in os.listdir(path) if i.endswith('html') or i.endswith('md')])
+
+    for local_notename in local_notenames:
+        markdown_path = os.path.join(path, '{}.md'.format(local_notename))
+        html_path = os.path.join(path, '{}.html'.format(local_notename))
+        png_path = os.path.join(path, '{}.png'.format(local_notename))
+
+        if os.path.exists(markdown_path):
+            markdown_stat = os.stat(markdown_path)
+
+            if os.path.exists(html_path):
+                html_stat = os.stat(html_path)
+                if markdown_stat.st_mtime > html_stat.st_mtime:
+                    print("{} 更新时间大于 {}".format(markdown_path, html_path))
+                    convert_to_html(local_notename, markdown_path, html_path)
+            else:
+                convert_to_html(local_notename, markdown_path, html_path)
+
+        if os.path.exists(html_path):
+            html_stat = os.stat(html_path)
+            if os.path.exists(png_path):
+                png_stat = os.stat(png_path)
+                if html_stat.st_mtime > png_stat.st_mtime:
+                    print("{} 更新时间大于 {}".format(html_path, png_path))
+                    convert_to_png(html_path, png_path)
+            else:
+                convert_to_png(html_path, png_path)
+
+        # png 一定存在
+
+        if local_notename in notes:
+            note = notes[local_notename]
             note = note_store.getNote(note.guid, True, True, True, True)
-            if file_stat.st_mtime > note.updated/10**3:
-                print("更新 {}".format(notename))
-                png_path = os.path.join(path, '{}.png'.format(notename))
-                convert_to_png(os.path.join(path, filename), png_path)
-                update_png_note(note, notename, png_path)
+            png_stat = os.stat(png_path)
+            if png_stat.st_mtime > note.updated/10**3:
+                print("更新 {}".format(local_notename))
+                markdown_path = os.path.join(path, '{}.md'.format(local_notename))
+                if not os.path.exists(markdown_path):
+                    markdown_path = None
+                update_note(note, local_notename, png_path, markdown_path, html_path)
         else:
-            print("创建 {}".format(notename))
-            png_path = os.path.join(path, '{}.png'.format(notename))
-            convert_to_png(os.path.join(path, filename), png_path)
-            create_png_note(notename, png_path, notebook)
+            print("创建 {}".format(local_notename))
+            if not os.path.exists(markdown_path):
+                markdown_path = None
+            convert_to_png(html_path, png_path)
+            create_note(local_notename, notebook, png_path, markdown_path, html_path)
 
 
-def cmd_to_evernotes(path):
+def sync_to_evernotes(path):
     """将cmdmarkdown的文件更新到evernote中去"""
     evernotebooks = {i.name:i for i in note_store.listNotebooks()}
     for notebook_name in os.listdir(path):
@@ -148,7 +208,7 @@ def cmd_to_evernotes(path):
 
 
 def main():
-    cmd_to_evernotes('./notes/')
+    sync_to_evernotes('./notes/')
 
 
 if __name__ == '__main__':
